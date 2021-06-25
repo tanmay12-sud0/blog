ff9bb84aefafdfa6603df88f0651302ed2d40f84
Tanmay <Tanmaygoyal888@gmail.com>
2021-06-25 11:16:04 +0530

    first

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import TaskList from './Components/TaskList'
+import Footer from './Components/Footer';
+import GlobalState from './Context/GlobalState';
 
-export default function App() {
-  return (
+export default class App extends React.Component{
+ render(){
+  return(
+   <GlobalState>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <TaskList />
+     <Footer/>
     </View>
+   </GlobalState>
   );
+ }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  flexDirection:'column',
+ }
 });
---
+++ src/IndexScreen.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import {Text, StyleSheet, Button, View, Image, ScrollView} from 'react-native';
+
+
+const IndexScreen = (props) => {
+  
+      return(
+          <View >
+          <Text>hello everyone</Text>
+          </View>
+      )
+  }
+  
+  const styles = StyleSheet.create({
+    
+  });
+  
+  export default IndexScreen;
